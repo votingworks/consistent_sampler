a84241dbed608829fb746246f95b0ab9216e6140
Jonah Kagan <jonah@voting.works>
2021-02-17 11:10:53 -0800

    Add test docstrings

--- code/test_consistent_sampler.py
+++ code/test_consistent_sampler.py
@@ -11,7 +11,15 @@ def ids(n):
 
 
 class TestConsistentSampler(unittest.TestCase):
+    """
+    Tests based on the paper describing consistent sampling: https://arxiv.org/abs/1808.10016
+    """
+
     def test_consistent_wrt_seed(self):
+        """
+        Ensure the sample results are always the same for the same seed, and
+        always different for different seeds.
+        """
         for n in range(1, 10):
             self.assertEqual(
                 list(sampler(ids(n), 12345)),
@@ -32,6 +40,16 @@ class TestConsistentSampler(unittest.TestCase):
             )
 
     def test_consistent_wrt_sample_size(self):
+        """
+        Ensure that when drawing a small sample and a larger from the same
+        list of ids, the small sample is a subset of the larger sample.
+
+        From the paper:
+            For any pool I and any seed u, we have that for any sample size s
+            and s' with s' ≥ s:
+                S(I, u, s) ⊆ S(I, u, s0)
+            so that a larger sample is just an extension of a smaller sample.
+        """
         for i in range(1, 10):
             for j in range(1, i):
                 self.assertEqual(
@@ -47,13 +65,24 @@ class TestConsistentSampler(unittest.TestCase):
                 )
 
     def test_consistent_wrt_population(self):
+        """
+        If we draw a sample from a pool and another sample from a subset of
+        that pool, the sample from the subset should equal exactly the items
+        from the sample from the larger pool that are members of the subset.
+
+        From the paper:
+        For any two nonempty sets J and K with J ⊆ K, we have
+            S(J , u) = S(K, u) ∩ J
+        where S ∩ J denotes the subsequence of sequence S obtained by
+        retaining only elements in J.
+        """
         n = 10
         for i in range(1, n):
             K = random.sample(ids(n), random.randint(1, i))
             J = random.sample(K, random.randint(1, len(K)))
             self.assertEqual(
                 list(sampler(J, 12345, output="id")),
-                [k for k in list(sampler(K, 12345, output="id")) if k in J],
+                [k for k in sampler(K, 12345, output="id") if k in J],
             )
             self.assertEqual(
                 list(
